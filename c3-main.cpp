bab5a80868f5de0d5dc9b36c6c7ba357f0faface
dboemer <dominik.boemer@gmail.com>
2023-12-27 14:17:45 +0000

    transform scan and update implemented

--- c3-main.cpp
+++ c3-main.cpp
@@ -99,6 +99,22 @@ void drawCar(Pose pose, int num, Color color, double alpha, pcl::visualization::
 	renderBox(viewer, box, num, color, alpha);
 }
 
+Eigen::Matrix4d NDT(pcl::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ> ndt, PointCloudT::Ptr source, Pose startingPose, int iterations){
+
+	Eigen::Matrix4f init_guess = transform3D(startingPose.rotation.yaw, startingPose.rotation.pitch, startingPose.rotation.roll, startingPose.position.x, startingPose.position.y, startingPose.position.z).cast<float>();
+
+  	// Setting max number of registration iterations.
+  	ndt.setMaximumIterations(iterations);
+	ndt.setInputSource(source);
+  	
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ndt(new pcl::PointCloud<pcl::PointXYZ>);
+  	ndt.align(*cloud_ndt, init_guess);
+
+	Eigen::Matrix4d transformation_matrix = ndt.getFinalTransformation().cast<double>();
+
+	return transformation_matrix;
+}
+
 int main(){
 
 	auto client = cc::Client("localhost", 2000);
@@ -213,16 +229,17 @@ int main(){
 			ndt.setTransformationEpsilon(.0001);
 			ndt.setStepSize(1);
 			ndt.setResolution(1);
-			ndt.setInputTarget(mapcloud);
+			ndt.setInputTarget(mapCloud);
 			Eigen::Matrix4d transform = NDT(ndt, cloudFiltered, pose, 3);
 			pose = getPose(transform);
 
 			// TODO: Transform scan so it aligns with ego's actual pose and render that scan
-			
+			PointCloudT::Ptr transformed_scan (new PointCloudT);
+			pcl::transformPointCloud(*cloudFiltered, *transformed_scan, transform);
 
 			viewer->removePointCloud("scan");
 			// TODO: Change `scanCloud` below to your transformed scan
-			renderPointCloud(viewer, scanCloud, "scan", Color(1,0,0) );
+			renderPointCloud(viewer, transformed_scan, "scan", Color(1,0,0));
 
 			viewer->removeAllShapes();
 			drawCar(pose, 1,  Color(0,1,0), 0.35, viewer);
