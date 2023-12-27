fcba3f7cf83c0d5d8e799f57608e7408d08f6eb7
Ubuntu <ubuntu@ip-172-31-10-140.us-west-2.compute.internal>
2023-12-27 13:52:24 +0000

    voxel filtering and NDT matching implemented

--- c3-main.cpp
+++ c3-main.cpp
@@ -201,11 +201,24 @@ int main(){
 			
 			new_scan = true;
 			// TODO: (Filter scan using voxel filter)
+			pcl::VoxelGrid<PointT> vg;
+			vg.setInputCloud(scanCloud);
+			double filterRes = 0.5;
+			vg.setLeafSize(filterRes, filterRes, filterRes);
+			typename pcl::PointCloud<PointT>::Ptr cloudFiltered (new pcl::PointCloud<PointT>);
+			vg.filter(*cloudFiltered);
 
 			// TODO: Find pose transform by using ICP or NDT matching
-			//pose = ....
+			pcl::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ> ndt;
+			ndt.setTransformationEpsilon(.0001);
+			ndt.setStepSize(1);
+			ndt.setResolution(1);
+			ndt.setInputTarget(mapcloud);
+			Eigen::Matrix4d transform = NDT(ndt, cloudFiltered, pose, 3);
+			pose = getPose(transform);
 
 			// TODO: Transform scan so it aligns with ego's actual pose and render that scan
+			
 
 			viewer->removePointCloud("scan");
 			// TODO: Change `scanCloud` below to your transformed scan
